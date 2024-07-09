ebcb998203a7d669cc30d3c962748f99109bb089
Husain Baghwala <156281491+walkoverinterns1@users.noreply.github.com>
2024-07-09 15:40:40 +0530

    error

--- src/db_services/ConfigurationServices.py
+++ src/db_services/ConfigurationServices.py
@@ -33,27 +33,51 @@ async def get_api_call_by_id(api_id):
             'error': "something went wrong!!"
         }
     
+# async def get_bridge_by_slugname(org_id, slug_name):
+#     try:
+#         print(111,org_id,222,slug_name)
+#         bridges = configurationModel.find_one({
+#             'slugName': slug_name,
+#             'org_id': 'husain_123'
+#         })
+#         print("hii hello", bridges)
+#         if bridges and 'responseRef' in bridges:
+#             # Populate 'responseRef' if it's a reference to another collection
+#             print("hii 3")
+#             response_ref = await db['responses'].find_one({'_id': bridges['responseRef']})
+#             bridges['responseRef'] = response_ref
+        
+#         return {
+#             'success': True,
+#             'bridges': bridges
+#         }
+#     except Exception as error:
+#         traceback.print_exc()
+#         print(f"error: {error}")
+#         return {
+#             'success': False,
+#             'error': "something went wrong!!"
+#         }
+
 async def get_bridge_by_slugname(org_id, slug_name):
     try:
-        bridges = await configurationModel.find_one({
+        org_id = 'husain_123'
+        bridge =  configurationModel.find_one({
             'slugName': slug_name,
             'org_id': org_id
         })
-        print("hii", bridges)
-        print("hi how are you 1")
-        if bridges and 'responseRef' in bridges:
-            # Populate 'responseRef' if it's a reference to another collection
-            print("hi how are you 2")
-            response_ref = await db['responses'].find_one({'_id': bridges['responseRef']})
-            bridges['responseRef'] = response_ref
-        
+
+        if bridge and 'responseRef' in bridge:
+            response_ref_id = bridge['responseRef']
+            response_ref = configurationModel.find_one({'_id': response_ref_id})
+            bridge['responseRef'] = response_ref
+
         return {
             'success': True,
-            'bridges': bridges
+            'bridges': bridge
         }
     except Exception as error:
-        traceback.print_exc()
-        print(f"error: {error}")
+        print("error:", error)
         return {
             'success': False,
             'error': "something went wrong!!"
