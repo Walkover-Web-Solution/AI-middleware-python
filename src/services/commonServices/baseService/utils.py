d1f753805112a2124089021019d8ad5e808e32d2
Harsh sahu <rohitmirchandani365@gmail.com>
2024-08-10 18:18:24 +0530

    ratelimit complete

--- src/services/commonServices/baseService/utils.py
+++ src/services/commonServices/baseService/utils.py
@@ -90,10 +90,10 @@ def tool_call_formatter(configuration: dict, service: str) -> dict:
         ]
 
 
-async def send_request(url, data, method, headers):
+def send_request(url, data, method, headers):
     try:
-        async with httpx.AsyncClient() as client:
-            response = await client.request(method, url, json=data, headers=headers)
+        with httpx.Client() as client:
+            response = client.request(method, url, json=data, headers=headers)
             return response
     except httpx.RequestError as error:
         print("send_request error=>", error)
