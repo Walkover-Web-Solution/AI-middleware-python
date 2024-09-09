98e1ed214b6af45afa5c87a61e5d3e64c61a28aa
Harsh sahu <harshksahu11@gmail.com>
2024-09-09 17:07:59 +0530

    fix duplicate bridge

--- src/services/commonServices/bridgeServices.py
+++ src/services/commonServices/bridgeServices.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from src.db_services.ConfigurationServices import get_bridges
 from datetime import datetime, timezone
 from src.controllers.configController import duplicate_create_bridges
+
 import json
 
 async def duplicate_bridge(request : Request):
@@ -26,14 +27,8 @@ async def duplicate_bridge(request : Request):
             "configuration": bridge.get('configuration'),
             "apikey": bridge.get('apikey'),
             "slugName": new_slugName,
-            # "created_at": new_created_at,
-            # "api_call": bridge.get('api_call'),
-            # "api_endpoints": bridge.get('api_endpoints'),
-            # "is_api_call": bridge.get('is_api_call'),
-            # "responseIds": bridge.get('responseIds'),
-            # "responseRef": bridge.get('responseRef'),
-            # "defaultQuestions": bridge.get('defaultQuestions'),
-            # "actions": bridge.get('actions')
+            "function_ids": bridge.get('function_ids'),
+            "actions": bridge.get('actions')
         })
         return JSONResponse(status_code=200, content={
             "success": True,
