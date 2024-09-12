be6404724667a3cb54b63d3f3ee066c9d14c1b41
Harsh sahu <harshksahu11@gmail.com>
2024-09-12 16:18:03 +0530

    fix api dis

--- src/services/utils/getConfiguration.py
+++ src/services/utils/getConfiguration.py
@@ -31,7 +31,7 @@ async def getConfiguration(configuration, service, bridge_id, apikey, template_i
         format = {
             "type": "function",
             "name": api_data.get("function_name", api_data.get("endpoint")),
-            "description": api_data.get("description", api_data.get("short_description")),
+            "description": api_data.get("description", api_data.get("api_description", api_data.get("short_description", ""))),
             "properties": { field: { "type": "string" } for field in api_data.get("required_params", api_data.get("required_fields"))  },
             "required": api_data.get("required_params", api_data.get("required_fields"))
         }
