4fb3541f927abec9917fd00722c9e5ad2f54a625
Harsh sahu <harshksahu11@gmail.com>
2024-09-24 17:00:37 +0530

    add console for test

--- src/services/commonServices/openAI/runModel.py
+++ src/services/commonServices/openAI/runModel.py
@@ -2,10 +2,11 @@ from .openAIInitializerService import OpenAIInitializer
 import traceback
 import json
 
-async def runModel(configuration, chat=True, apiKey=None):
+async def runModel(configuration, chat=True, apiKey=None, bridge_id= ''):
     try:
         OpenAIConfig = OpenAIInitializer(apiKey)
         openAI = OpenAIConfig.getOpenAIService()
+        print(11, json.dumps(configuration), 22, bridge_id)
         if chat:
             chat_completion = openAI.chat.completions.create(**configuration)
             response = chat_completion.to_dict()
