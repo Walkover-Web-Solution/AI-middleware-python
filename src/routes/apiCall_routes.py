4445801a393afbcd3723a0a3430885116b1749ea
parakhjain23 <parakhjain2301@gmail.com>
2024-09-27 11:45:06 +0530

    function url bug fix

--- src/routes/apiCall_routes.py
+++ src/routes/apiCall_routes.py
@@ -8,6 +8,6 @@ router = APIRouter()
 async def get_all_apicalls(request: Request):
     return await get_all_apicalls_controller(request)
 
-@router.put('/', dependencies=[Depends(jwt_middleware)])
-async def update_apicalls(request: Request):
-    return await update_apicalls_controller(request)
+@router.put('/{function_id}', dependencies=[Depends(jwt_middleware)])
+async def update_apicalls(request: Request, function_id: str):
+    return await update_apicalls_controller(request, function_id)
