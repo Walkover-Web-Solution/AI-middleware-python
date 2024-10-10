706cf1341f46be8a5ff99e4a1557c9766f76e6b2
Husain Baghwala <husainbw123@gmail.com>
2024-10-10 17:42:15 +0530

    run_in_executor_revert_for_send_message

--- src/routes/chatBot_routes.py
+++ src/routes/chatBot_routes.py
@@ -4,6 +4,7 @@ from concurrent.futures import ThreadPoolExecutor
 from config import Config
 from src.middlewares.interfaceMiddlewares import send_data_middleware, chat_bot_auth, reset_chatBot
 from src.middlewares.ratelimitMiddleware import rate_limit
+from src.handler.executionHandler import handle_exceptions
 
 router = APIRouter()
 executor = ThreadPoolExecutor(max_workers= int(Config.max_workers) or 10)
@@ -13,8 +14,13 @@ async def auth_and_rate_limit(request: Request):
     await rate_limit(request, key_path='profile.user.id')
 
 @router.post("/{botId}/sendMessage", dependencies=[Depends(auth_and_rate_limit)])
+@handle_exceptions
 async def send_message(request: Request, botId: str):
-   result = await send_data_middleware(request, botId)
+   # Get the current event loop
+   loop = asyncio.get_event_loop()
+
+    # Run the async function in a separate thread to avoid blocking
+   result = await loop.run_in_executor(executor, lambda: asyncio.run(send_data_middleware(request, botId)))
    return result
 
 @router.post("/{botId}/resetchat", dependencies=[Depends(auth_and_rate_limit)])
