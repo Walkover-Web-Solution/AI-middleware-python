95ab1e544d8a5d61447775c69acf9381060687a5
Husain Baghwala <husainbw123@gmail.com>
2024-10-14 13:21:29 +0530

    bariables_path_updated_for_anthropic

--- src/services/commonServices/baseService/baseService.py
+++ src/services/commonServices/baseService/baseService.py
@@ -225,26 +225,38 @@ class BaseService:
             raise ValueError(f"error occurs from openAi api {e.args[0]}")
 
     async def replace_variables_in_args(self, modal_response):
-        if self.service == 'openai' or self.service == 'groq':
-            variables = self.variables
-            variables_path = self.variables_path
-            if variables_path is None or isinstance(variables_path, list):
-                return modal_response      
-            tool_calls = modal_response.get('choices', [])[0].get('message', {}).get("tool_calls", [])
-
-            for index, tool_call in enumerate(tool_calls):
+        variables = self.variables
+        variables_path = self.variables_path
+        if variables_path is None or isinstance(variables_path, list):
+            return modal_response     
+        service_map = {
+        'openai': lambda response: response.get('choices', [])[0].get('message', {}).get('tool_calls', []),
+        'groq': lambda response: response.get('choices', [])[0].get('message', {}).get('tool_calls', []),
+        'anthropic': lambda response: response.get('content', [])
+        } 
+        tool_calls = service_map.get(self.service, lambda x: [])(modal_response)
+        for index, tool_call in enumerate(tool_calls):
+            if self.service == 'openai' or self.service == 'groq':
                 args = json.loads(tool_call['function']['arguments'])
                 function_name = tool_call['function']['name']
-                if function_name in variables_path:
-                    function_variables_path = variables_path[function_name]
-                    if isinstance(function_variables_path, list):
-                        continue
-                    for key, path in function_variables_path.items():
-                        value_to_set = _.objects.get(variables, path)
+            elif self.service == 'anthropic':
+                if tool_call['type'] == 'text':
+                    continue
+                args = tool_call['input']
+                function_name = tool_call['name']
+            if function_name in variables_path:
+                function_variables_path = variables_path[function_name]
+                if isinstance(function_variables_path, list):
+                    continue
+                for key, path in function_variables_path.items():
+                    value_to_set = _.objects.get(variables, path)
 
-                        if value_to_set is not None:
-                            _.objects.set_(args, key, value_to_set)
+                    if value_to_set is not None:
+                        _.objects.set_(args, key, value_to_set)
 
+            if self.service == 'openai' or self.service == 'groq':
                 tool_call['function']['arguments'] = json.dumps(args)
+            elif self.service == 'anthropic':
+                tool_call['input'] = args
 
         return modal_response
