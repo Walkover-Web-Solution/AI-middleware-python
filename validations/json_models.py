052b48ac6f83359ce043dbe6fbeb087bfa50e9be
ViaSocket-Git <abbassadri786@gmail.com>
2024-10-16 16:31:13 +0530

    Suggestions-Groq_Model

--- validations/json_models.py
+++ validations/json_models.py
@@ -6,8 +6,12 @@ def check_json_support(model, service):
                           return True
                     else:
                           return False
+                case  'groq': 
+                    if model in ['llama-3.1-70b-versatile', 'llama-3.1-8b-instant', 'llama3-groq-70b-8192-tool-use-preview','llama3-groq-8b-8192-tool-use-preview','llama3-70b-8192','llama3-8b-8192','mixtral-8x7b-32768','gemma-7b-it','gemma2-9b-it']:
+                          return True
+                    else:
+                          return False
                 # case 'anthropic':
                 #     if model in ['claude-3-5-sonnet-20240620']:
                 #           return True
-                # case  'groq':
-                #     pass
+                
