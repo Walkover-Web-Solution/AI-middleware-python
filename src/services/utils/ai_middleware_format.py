f5bf85c8845e69b11a34260b81370801630adfb5
Harsh sahu <harshksahu11@gmail.com>
2024-10-24 17:42:12 +0530

    send alert when break

--- src/services/utils/ai_middleware_format.py
+++ src/services/utils/ai_middleware_format.py
@@ -1,3 +1,4 @@
+from src.services.utils.apiservice import fetch
 async def  Response_formatter(response, service):
     if service == 'openai':
         return {
@@ -50,3 +51,11 @@ async def  Response_formatter(response, service):
                 "total_tokens" : response.get("usage", {}).get("total_tokens", None)
             }
         }
+
+async def validateResponse(final_response,configration,bridgeId):
+    parsed_data = final_response.get("data",{}).get("content","").replace(" ", "").replace("\n", "")
+    if(parsed_data == ''):
+        await send_alert(data={"final_response":final_response,"configration":configration,"bridgeId":bridgeId})
+
+async def send_alert(data):
+    await fetch("https://flow.sokt.io/func/scriYP8m551q",method='POST',json_body=data)
