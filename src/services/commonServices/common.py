affd0853b26691228699f85006f76b31ccc55506
parakhjain23 <parakhjain2301@gmail.com>
2024-10-26 12:27:40 +0530

    custom config for rich text

--- src/services/commonServices/common.py
+++ src/services/commonServices/common.py
@@ -187,6 +187,9 @@ async def chat(request: Request):
                     if params["configuration"].get('conversation'):
                         del params["configuration"]['conversation']
                     model_response_content = result.get('historyParams').get('message')
+                    # custom config for the rich text
+                    params['customConfig']['response_type'] = {"type": "json_object"}
+                    params['customConfig']['max_tokens'] = modelConfig['max_tokens']['max']
                     obj = await create_service_handler(params,service)
                     newresult = await obj.execute()
                     tokens = obj.calculate_usage(newresult["modelResponse"])
