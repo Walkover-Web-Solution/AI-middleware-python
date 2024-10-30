a8582177dad1436d0958c84bec613d2d47ef28b0
harsh <145131947+harshhsahu@users.noreply.github.com>
2024-10-30 12:45:29 +0530

    adding comment for log

--- src/middlewares/middleware.py
+++ src/middlewares/middleware.py
@@ -2,7 +2,7 @@ import jwt
 from fastapi import Request, HTTPException
 import traceback
 from config import Config
-
+import json
 from src.services.utils.apiservice import fetch
 from src.services.utils.time import Timer
 
@@ -30,7 +30,7 @@ async def jwt_middleware(request: Request):
             timer = Timer()
             timer.start()
             request.state.timer = timer
-            
+            print(f"{json.dumps(request.headers)} check_token 111111")
             if request.headers.get('Authorization') :
                 token = request.headers.get('Authorization')
                 if not token:
@@ -40,8 +40,8 @@ async def jwt_middleware(request: Request):
                 # request.headers.get('proxy_auth_token')
                 check_token = await make_data_if_proxy_token_given(request)
 
+            print(check_token ,"check_token 111111")
             if check_token:
-                print(f"{check_token} check_token 111111")
                 check_token['org']['id'] = str(check_token['org']['id'])
                 request.state.profile = check_token
                 request.state.org_id = str(check_token.get('org', {}).get('id'))
