8943e8638b9fec706b978ca0896acad2c6f05b41
parakhjain23 <parakhjain2301@gmail.com>
2024-11-08 18:01:21 +0530

    logger

--- src/services/utils/logger.py
+++ src/services/utils/logger.py
@@ -1,6 +1,13 @@
 import logging
 
-logging.basicConfig(format='%(levelname)s - %(message)s')
+# logging.basicConfig(format='%(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(),           # Outputs to console
+    ]
+)
 def get_logger() -> logging.Logger:
     logger = logging.getLogger()
     return logger
