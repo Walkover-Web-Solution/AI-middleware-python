a6d08af2312ae51b300a8d7746a7526ac87e8977
Parakh Jain <97786329+parakhjain23@users.noreply.github.com>
2024-11-18 18:41:36 +0530

    optimize proompt (#237)

--- src/controllers/bridgeController.py
+++ src/controllers/bridgeController.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, Depends, Request
 from ..middlewares.middleware import jwt_middleware
-import traceback
-from src.services.commonServices.bridgeServices import duplicate_bridge
+from src.services.commonServices.bridgeServices import duplicate_bridge, optimize_prompt_controller
 router = APIRouter()
 
 @router.post('/duplicate',dependencies=[Depends(jwt_middleware)])
 async def duplicate_bridges(request: Request):
     return await duplicate_bridge(request)
+
+
+@router.post('/{bridge_id}/optimize/prompt', dependencies=[Depends(jwt_middleware)])
+async def update_apicalls(request: Request, bridge_id: str):
+    return await optimize_prompt_controller(request, bridge_id)
