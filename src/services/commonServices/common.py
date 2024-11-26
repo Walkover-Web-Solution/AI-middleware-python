d198365f9c94caafef987748c3f044fe3079710f
harsh <145131947+harshhsahu@users.noreply.github.com>
2024-11-26 13:41:18 +0530

    Stucture output (#254)
    * stucture out put support added
    
    * fix stucture output

--- src/services/commonServices/common.py
+++ src/services/commonServices/common.py
@@ -65,9 +65,10 @@ async def chat(request_body):
     usage = {}
     customConfig = {}
     response_format = configuration.get("response_format")
-    if(response_format):
-        if response_format.type == 'json_schema':
-            response_format.type = 'json_schema' if response_format['json_schema'] else 'json_object'
+    response_type = configuration.get("response_type")
+    if(response_type):
+        if response_type.get("type") == 'json_schema':
+            response_type['type'] = 'json_schema' if response_type['json_schema'] else 'json_object'
     model = configuration.get('model')
     is_playground = state['is_playground']
     bridge = body.get('bridge')
