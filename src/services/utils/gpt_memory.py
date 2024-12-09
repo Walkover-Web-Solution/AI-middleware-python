f2d1970c630999c9eeeee7e06fecd357444fcc79
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2024-12-09 14:06:47 +0530

    gpt_memory_user_context (#296)

--- src/services/utils/gpt_memory.py
+++ src/services/utils/gpt_memory.py
@@ -1,8 +1,8 @@
 from .apiservice import fetch
 
-async def handle_gpt_memory(id, user, assistant, purpose):
+async def handle_gpt_memory(id, user, assistant, purpose, gpt_memory_context):
     try:
-        variables = {'threadID': id, 'memory' : purpose}
+        variables = {'threadID': id, 'memory' : purpose, "gpt_memory_context": gpt_memory_context}
         content = assistant.get('data', {}).get('content', "")
         response, rs_headers = await fetch(
             f"https://proxy.viasocket.com/proxy/api/1258584/29gjrmh24/api/v2/model/chat/completion",
@@ -17,7 +17,8 @@ async def handle_gpt_memory(id, user, assistant, purpose):
                 "user": "use the function to store the memory if the user message and history is related to the context or is important to store else don't call the function and ignore it.",
                 "bridge_id": "6752d9fc232e8659b2b65f0d",
                 "response_type": "text",
-                "variables": variables
+                "variables": variables,
+                "thread_id" : id
             }
         )
         if not response.get('success', True):
