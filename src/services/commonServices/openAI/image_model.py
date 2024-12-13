8ea2c2d91e6aa1efc953fb48b7914f8a4cb56c76
Husain Baghwala <husainbw123@gmail.com>
2024-12-13 14:04:50 +0530

    review chnages

--- src/services/commonServices/openAI/image_model.py
+++ src/services/commonServices/openAI/image_model.py
@@ -7,6 +7,7 @@ import uuid
 import json
 from config import Config
 from google.oauth2 import service_account
+from src.services.utils.apiservice import fetch
 
 async def OpenAIImageModel(configuration, apiKey, execution_time_logs, timer):
     try:
@@ -17,8 +18,7 @@ async def OpenAIImageModel(configuration, apiKey, execution_time_logs, timer):
         response = chat_completion.to_dict()
         timer.start()
         image_url = response['data'][0]['url']
-        image_response = requests.get(image_url)
-        image_content = BytesIO(image_response.content)
+        image_content, headers = await fetch(url=image_url, method='GET', image=True)
 
         credentials_dict = json.loads(Config.GCP_CREDENTIALS)
         credentials = service_account.Credentials.from_service_account_info(credentials_dict)
