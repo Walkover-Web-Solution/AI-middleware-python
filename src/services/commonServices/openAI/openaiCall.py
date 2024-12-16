abe7e33e7e9117834171e04da8ff475182729f64
Husain Baghwala <husainbw123@gmail.com>
2024-12-16 15:48:05 +0530

    token_calculation_calss_added

--- src/services/commonServices/openAI/openaiCall.py
+++ src/services/commonServices/openAI/openaiCall.py
@@ -43,8 +43,8 @@ class UnifiedOpenAICase(BaseService):
                     raise ValueError(functionCallRes.get('error'))
                 self.update_model_response(modelResponse, functionCallRes)
                 tools = functionCallRes.get("tools", {})
-            usage = self.calculate_usage(modelResponse)
             if not self.playground:
+                usage = self.token_calculator.calculate_usage(modelResponse)
                 historyParams = self.prepare_history_params(modelResponse, tools)
         return {'success': True, 'modelResponse': modelResponse, 'historyParams': historyParams, 'usage': usage }
     
---
+++ src/services/utils/token_calculation.py
@@ -0,0 +1,40 @@
+import pydash as _
+
+class TokenCalculator:
+    def __init__(self, service, model_output_config):
+        self.service = service
+        self.model_output_config = model_output_config
+        self.total_usage = {
+            "totalTokens": 0,
+            "inputTokens": 0,
+            "outputTokens": 0,
+            "expectedCost": 0.0
+        }
+
+    def calculate_usage(self, model_response):
+        usage = {}
+        match self.service:
+            case 'openai' | 'groq':
+                usage["totalTokens"] = _.get(model_response, self.model_output_config['usage'][0]['total_tokens'])
+                usage["inputTokens"] = _.get(model_response, self.model_output_config['usage'][0]['prompt_tokens'])
+                usage["outputTokens"] = _.get(model_response, self.model_output_config['usage'][0]['completion_tokens'])
+                usage["expectedCost"] = (usage['inputTokens'] / 1000 * self.model_output_config['usage'][0]['total_cost']['input_cost']) + (usage['outputTokens'] / 1000 * self.model_output_config['usage'][0]['total_cost']['output_cost'])
+            case 'anthropic':
+                usage["inputTokens"] = _.get(model_response, self.model_output_config['usage'][0]['prompt_tokens'])
+                usage["outputTokens"] = _.get(model_response, self.model_output_config['usage'][0]['completion_tokens'])
+                usage["totalTokens"] = usage["inputTokens"] + usage["outputTokens"]
+                
+            case _:
+                pass
+
+        self._update_total_usage(usage)
+        return usage
+
+    def _update_total_usage(self, usage):
+        self.total_usage["totalTokens"] += usage.get("totalTokens", 0)
+        self.total_usage["inputTokens"] += usage.get("inputTokens", 0)
+        self.total_usage["outputTokens"] += usage.get("outputTokens", 0)
+        self.total_usage["expectedCost"] += usage.get("expectedCost", 0.0)
+
+    def get_total_usage(self):
+        return self.total_usage
