526944beebf4d455ff8fda957dda624e078ebf18
Husain Baghwala <husainbw123@gmail.com>
2024-12-23 11:21:40 +0530

    viion_support_master

--- src/services/commonServices/common.py
+++ src/services/commonServices/common.py
@@ -101,6 +101,7 @@ async def chat(request_body):
     memory = None
     version_id = body.get('version_id')
     gpt_memory_context = body.get('gpt_memory_context')
+    images = body.get('images')
     
     if model == 'o1-preview' or model == 'o1-mini':
         reasoning_model = True
@@ -199,7 +200,8 @@ async def chat(request_body):
             "reasoning_model" : reasoning_model,
             "memory": memory,
             "type" : type,
-            "token_calculator" : token_calculator
+            "token_calculator" : token_calculator,
+            "images" : images
 
         }
         class_obj = await create_service_handler(params,service)
