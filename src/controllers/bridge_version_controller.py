45cf32b4b4ec252458ed3797573f3741530fe990
Viasocket Interns <developers@whozzat.com>
2024-12-25 12:22:13 +0530

    Discard_Version_Fix-test (#345)
    * Discard_Version_Fix-main
    
    * PR Fix
    
    ---------
    
    Co-authored-by: ViaSocket-Git <abbassadri786@gmail.com>

--- src/controllers/bridge_version_controller.py
+++ src/controllers/bridge_version_controller.py
@@ -65,7 +65,7 @@ async def discard_version(request, version_id):
     bridge_id = body.get('bridge_id')
 
     bridge_data = await get_bridges_with_tools(bridge_id, org_id)
-    bridge_data['bridges'] = {key: value for key, value in bridge_data['bridges'].items() if key not in ['name', 'slugName', 'bridgeType', '_id', 'versions']}
+    bridge_data['bridges'] = {key: value for key, value in bridge_data['bridges'].items() if key not in ['name', 'slugName', 'bridgeType', '_id', 'versions','status']}
     bridge_data['bridges']['is_drafted'] = False
     function_ids = bridge_data['bridges'].get('function_ids', [])
     if function_ids is not None:
