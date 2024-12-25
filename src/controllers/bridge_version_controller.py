4262f50f2cdffa5331a5243369a52e4a9f09030d
ViaSocket-Git <abbassadri786@gmail.com>
2024-12-25 12:13:12 +0530

    PR Fix

--- src/controllers/bridge_version_controller.py
+++ src/controllers/bridge_version_controller.py
@@ -65,12 +65,11 @@ async def discard_version(request, version_id):
     bridge_id = body.get('bridge_id')
 
     bridge_data = await get_bridges_with_tools(bridge_id, org_id)
-    bridge_data['bridges'] = {key: value for key, value in bridge_data['bridges'].items() if key not in ['name', 'slugName', 'bridgeType', '_id', 'versions']}
+    bridge_data['bridges'] = {key: value for key, value in bridge_data['bridges'].items() if key not in ['name', 'slugName', 'bridgeType', '_id', 'versions','status']}
     bridge_data['bridges']['is_drafted'] = False
     function_ids = bridge_data['bridges'].get('function_ids', [])
     if function_ids is not None:
         bridge_data['bridges']['function_ids'] = [ObjectId(fid) for fid in function_ids]
-    del bridge_data['bridges']['status']
     result = await update_bridge(version_id=version_id, update_fields=bridge_data['bridges'])
     if 'success' in result:
         return JSONResponse({"success": True, "message": "version changes discarded successfully", "version_id": version_id})
