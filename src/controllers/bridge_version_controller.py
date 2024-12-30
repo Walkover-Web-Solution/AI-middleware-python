ce0b4b035f8ad16e072b6b7062fd327c92d5edfb
Viasocket Interns <developers@whozzat.com>
2024-12-30 15:03:42 +0530

    Version_description-test (#335)
    * Version_description-test
    
    * Update_Bridge fixed
    
    * Description Removed from DBService
    
    * donot draft true if the version description is updated
    
    ---------
    
    Co-authored-by: ViaSocket-Git <abbassadri786@gmail.com>
    Co-authored-by: Natwar589 <natwaarrathor16082003@gmail.com>

--- src/controllers/bridge_version_controller.py
+++ src/controllers/bridge_version_controller.py
@@ -6,25 +6,27 @@ from src.services.utils.helper import Helper
 from ..db_services.ConfigurationServices import get_bridges_with_tools, update_bridge, get_bridges_without_tools
 from bson import ObjectId
 async def create_version(request):
-    try:
-       body = await request.json()
-       version_id = body.get('version_id')
-       org_id = request.state.profile['org']['id']
-       bridge_data = await get_bridges_without_tools(org_id=org_id, version_id= version_id)
-       if bridge_data is None:
-           return JSONResponse({"success": False, "message": "no version found"})
-       parent_id = bridge_data.get('bridges').get('parent_id')
-       create_new_version = await create_bridge_version(bridge_data.get('bridges'), parent_id=parent_id)
-       update_fields = {'versions' : [create_new_version]}
-       await update_bridges(parent_id, update_fields)
-       return {
-           "success": True,
-           "message" : "version created successfully",
-           "version_id" : create_new_version
-       }
-
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e)
+   try:
+      body = await request.json()
+      version_id = body.get('version_id')
+      version_description = body.get('version_description') or ""
+      org_id = request.state.profile['org']['id']
+      bridge_data = await get_bridges_without_tools(org_id=org_id, version_id= version_id)
+      if bridge_data is None:
+          return JSONResponse({"success": False, "message": "no version found"})
+      parent_id = bridge_data.get('bridges').get('parent_id')
+      bridge_data['bridges']['version_description'] = version_description
+      create_new_version = await create_bridge_version(bridge_data.get('bridges'), parent_id=parent_id)
+      update_fields = {'versions' : [create_new_version]}
+      await update_bridges(parent_id, update_fields)
+      return {
+          "success": True,
+          "message" : "version created successfully",
+          "version_id" : create_new_version
+      }
+   
+   except Exception as e:
+       raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e)
 
 async def get_version(request, version_id: str):
     try:
