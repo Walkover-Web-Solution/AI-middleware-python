6582d236b941966cf9b849df7a4b6b8da3bd466f
Husain Baghwala <husainbw123@gmail.com>
2025-01-18 10:49:13 +0530

    default varaibles added

--- src/services/commonServices/common.py
+++ src/services/commonServices/common.py
@@ -12,7 +12,7 @@ from ..utils.send_error_webhook import send_error_to_webhook
 import json
 from src.handler.executionHandler import handle_exceptions
 from models.mongo_connection import db
-from src.services.utils.common_utils import parse_request_body, initialize_timer, load_model_configuration, handle_pre_tools, handle_fine_tune_model,manage_threads, prepare_prompt, configure_custom_settings, build_service_params, process_background_tasks
+from src.services.utils.common_utils import parse_request_body, initialize_timer, load_model_configuration, handle_pre_tools, handle_fine_tune_model,manage_threads, prepare_prompt, configure_custom_settings, build_service_params, process_background_tasks, add_default_variables
 from src.services.utils.rich_text_support import process_chatbot_response
 app = FastAPI()
 from src.services.utils.helper import Helper
@@ -25,6 +25,8 @@ async def chat(request_body):
     try:
         # Step 1: Parse and validate request body
         parsed_data = parse_request_body(request_body)
+
+        parsed_data['variables'] = add_default_variables(parsed_data['variables'])
         
         # Step 2: Initialize Timer
         timer = initialize_timer(parsed_data['state'])
