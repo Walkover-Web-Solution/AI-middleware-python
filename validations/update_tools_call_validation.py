10220718c1a5785628268183f6bc835982f6bde7
Harsh sahu <harshksahu11@gmail.com>
2025-01-24 12:55:31 +0530

    remove uneless thing

--- validations/update_tools_call_validation.py
+++ validations/update_tools_call_validation.py
@@ -18,18 +18,14 @@ class internal_fields_model(BaseModel):
     parameter: Optional[Dict[str, internal_parameter_model]]
     
 class data_to_update_model(BaseModel):
-    activated: bool
     bridge_ids: Optional[List[str]]=[]
-    code: str
     created_at: datetime
     description: str
     endpoint_name: Optional[str]
     fields: Dict[str, internal_fields_model]
     function_name: str
-    is_python: int
     org_id: str
     required_params: Optional[List[str]]=[]
-    status: int
     updated_at: datetime
     version: str
     
