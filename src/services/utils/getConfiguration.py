95c24dc5097148684117d10310bc683d284d055c
Harsh sahu <harshksahu11@gmail.com>
2025-02-11 12:28:04 +0530

    double to single

--- src/services/utils/getConfiguration.py
+++ src/services/utils/getConfiguration.py
@@ -30,7 +30,7 @@ async def getConfiguration(configuration, service, bridge_id, apikey, template_i
         # if status is paused then only don't include it in tools
         name_of_function = makeFunctionName(api_data.get('endpoint_name') or  api_data.get("function_name"))
         tool_id_and_name_mapping[name_of_function] =  {
-            "url": f"https://flow.sokt.io/func/{api_data.get("function_name")}",
+            "url": f"https://flow.sokt.io/func/{api_data.get('function_name')}",
             "headers":{}
         }
         if api_data.get('status') == 0 and not name_of_function:
