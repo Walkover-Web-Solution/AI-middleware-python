a5ace3de2486a296eedfa4b1eb8ba75f13c1ec37
Harsh sahu <harshksahu11@gmail.com>
2025-03-10 19:37:32 +0530

    MakeConversationsAsPerService in testcases

--- src/services/utils/testcase_utils.py
+++ src/services/utils/testcase_utils.py
@@ -1,11 +1,91 @@
 from src.services.commonServices.baseService.baseService import BaseService
 import json
+import uuid
 
 def add_prompt_and_conversations(custom_config, conversations, service, prompt):
     custom_config['messages'] = custom_messages(custom_config, conversations, service, prompt)
     base_service = BaseService({})
     return base_service.service_formatter(custom_config, service)
-    
+
+def MakeConversationsAsPerService(conversations, service):
+    Newconversations = []
+    for conversation in conversations:    
+        match service:
+            case 'openai' | 'groq':
+                if conversation.role == 'tools_call':
+                    id =  f"call_{uuid.uuid4().hex[:6]}"
+                    for i, tools in enumerate(conversation.content):
+                        convers = {
+                            "role": "assistant",
+                            "content": None,
+                            "tool_calls": [
+                            {
+                                "id": f"{id}{i}{j}",
+                                "type": "function",
+                                "function": {
+                                "name": tool.name,
+                                "arguments": json.dumps(tool.args)
+                                }
+                            } for j, tool in enumerate(tools)
+                            ]
+                        }
+                        Newconversations.append(convers)
+                        for i, tools in enumerate(conversation.content):
+                             for j, tool in enumerate(tools):
+                                conversResponse = {
+                                    "role": "tool",
+                                    "content": json.dumps({
+                                            "response": tool.response,
+                                            "metadata": tool.metadata,
+                                            "status": tool.status
+                                        }),
+                                    "tool_call_id": f"{id}{i}{j}"
+                                }
+                                Newconversations.append(conversResponse)
+                else:
+                    Newconversations.append(conversation)
+            case 'anthropic':
+                if conversation.role == 'tools_call':
+                    id =  f"toolu_{uuid.uuid4().hex[:6]}"
+                    for i, tools in enumerate(conversation.content):
+                        convers = {
+                            "role": "assistant",
+                            "content":  [
+                                    {
+                                        "type": "text",
+                                        "text": "Call the function for better response"
+                                    },
+                            {
+                                "id": f"{id}{i}{j}",
+                                "type": "tool_use",
+                                "name": tool.name,
+                                "input": tool.args
+                            } for j, tool in enumerate(tools)
+                            ]
+                        }
+                        Newconversations.append(convers)
+                        for i, tools in enumerate(conversation.content):
+                            conversResponse = {
+                                "role": "user",
+                                "content": [
+                                    {
+                                        "id": f"{id}{i}{j}",
+                                        "type": "tool_result",
+                                        "content": json.dumps({
+                                            "response": tool.response,
+                                            "metadata": tool.metadata,
+                                            "status": tool.status
+                                        })
+                                    } for j, tool in enumerate(tools)
+                                ]
+                            }
+                            Newconversations.append(conversResponse)
+                else:
+                    Newconversations.append(conversation)
+
+    return Newconversations
+
+
 def custom_messages(custom_config, conversations, service, prompt):
     messages = []
     match service:
