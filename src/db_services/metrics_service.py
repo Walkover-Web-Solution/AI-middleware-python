cd5151d16d846e7545c4b70f0e1b094623ca91d3
Husain Baghwala <husainbw123@gmail.com>
2025-03-20 15:46:39 +0530

    bug fix

--- src/db_services/metrics_service.py
+++ src/db_services/metrics_service.py
@@ -98,12 +98,11 @@ async def create(dataset, history_params, version_id, send_error_to_webhook):
         ]
         cache_key = f"metrix_bridges{history_params['bridge_id']}"
         oldTotalToken = await find_in_cache(cache_key) or 0 
-        print("hii",oldTotalToken, "hiii i am here...")
         totaltoken = sum(data_object.get('totalTokens', 0) for data_object in dataset) + oldTotalToken
-        await store_in_cache(cache_key, float(totaltoken))
-        await send_error_to_webhook(history_params['bridge_id'], history_params['org_id'],totaltoken , 'metrix_limit_reached')
         await insertRawData(insert_ai_data_in_pg)
         await timescale_metrics(metrics_data)
+        await send_error_to_webhook(history_params['bridge_id'], history_params['org_id'],totaltoken , 'metrix_limit_reached')
+        await store_in_cache(cache_key, float(totaltoken))
     except Exception as error:
         traceback.print_exc()
         print('Error during bulk insert of Ai middleware', error)
