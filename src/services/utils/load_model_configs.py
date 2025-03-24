c3933ce17cea35f22e5cbd64e5d137089a997c1e
Husain Baghwala <husainbw123@gmail.com>
2025-03-24 15:51:22 +0530

    ..

--- src/services/utils/load_model_configs.py
+++ src/services/utils/load_model_configs.py
@@ -6,7 +6,7 @@ async def get_model_configurations():
     try:
         configurations = await modelConfigModel.find({}, {"_id": 0}).to_list(length=None)
         # Transform the list of configurations into a dictionary keyed by model name
-        config_dict = {conf["model"]: conf for conf in configurations}
+        config_dict = {conf["model_name"]: conf for conf in configurations}
         return config_dict
     except Exception as error:
         print(f"Error fetching model configurations: {error}")
