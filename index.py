671095f99f3c0c04f91bae0cfe99989e489360d7
Akshat Vyas <akshatvyas@Akshats-iMac.local>
2025-04-01 13:58:48 +0530

    adding space, just for testing build speed

--- index.py
+++ index.py
@@ -133,7 +133,7 @@ app.include_router(rag_routes,prefix="/rag")
 app.include_router(Internal_routes,prefix="/internal")
 app.include_router(testcase_routes, prefix='/testcases')
 
-
+ 
 if __name__ == "__main__":
     PORT = int(Config.PORT)
     uvicorn.run(app, host="0.0.0.0", port=PORT, log_config=settings.LOGGING_CONFIG, log_level="info")
