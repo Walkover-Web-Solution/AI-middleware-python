94fbbe7c2b831ad68a014791ae4a576fc2d910ce
Husain Baghwala <husainbw123@gmail.com>
2025-04-16 12:09:20 +0530

    bug-structred-output

--- src/services/utils/formatter/ai_middleware_chat_api.py
+++ src/services/utils/formatter/ai_middleware_chat_api.py
@@ -7,7 +7,7 @@ async def structured_output_optimizer(request):
         body = await request.json()
         variables = {'json_schema': body.get('json_schema')}
         user = 'create the json shcmea accroding to the dummy json explained in system prompt.'
-        result = await call_ai_middleware(user, bridge_id = bridge_ids['structured_output_optimizer'], varaibles = variables)
+        result = await call_ai_middleware(user, bridge_id = bridge_ids['structured_output_optimizer'], variables = variables)
         return result
     except Exception as err:
         print("Error calling function=>", err)
