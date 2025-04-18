6557d7c562f2d368abcf67d1eb84ab94d5a3f302
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2025-04-18 11:52:18 +0530

    advance paramters keys not changing is we update service (#590)

--- src/services/utils/getDefaultValue.py
+++ src/services/utils/getDefaultValue.py
@@ -12,18 +12,30 @@ async def get_default_values_controller(service, model, current_configuration, t
             config_items = config.get('configuration', {})
             
             for key, value in config_items.items():
-            
-                if current_configuration.get(key) == "min":
-                    default_values[key] = 'min' 
-                elif current_configuration.get(key) == "max":
+                current_value = current_configuration.get(key)
+                
+                if current_value == "min":
+                    default_values[key] = 'min'
+                elif current_value == "max":
                     default_values[key] = 'max'
-                elif current_configuration.get(key) == 'default':
+                elif current_value == 'default':
                     if type == 'embedding':
                         default_values[key] = config_items[key]['default']
                     else:
                         default_values[key] = 'default'
                 else:
-                    default_values[key] = value.get('default', None) if key == 'model' or type == 'embedding' else 'default'
+                    if key in config_items:
+                        min_value = value.get('min')
+                        max_value = value.get('max')
+                        if min_value is not None and max_value is not None:
+                            if not (min_value <= current_value <= max_value):
+                                default_values[key] = value.get('default', None)
+                            else:
+                                default_values[key] = current_value
+                        else:
+                            default_values[key] = current_value
+                    else:
+                        default_values[key] = value.get('default', None) if key == 'model' or type == 'embedding' else 'default'
             
             return default_values
 
