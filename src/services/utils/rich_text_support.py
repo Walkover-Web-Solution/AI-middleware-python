fa708a0880100f742f4cae9a48b7e1bb7b1abf07
Natwar589 <natwarrathor16082003@gmail.com>
2025-05-10 18:47:55 +0530

    bug solve for rich text

--- src/services/utils/rich_text_support.py
+++ src/services/utils/rich_text_support.py
@@ -3,7 +3,7 @@ import pydash as _
 import uuid
 from src.configs.constant import bridge_ids
 from .ai_call_util import call_ai_middleware
-
+import json
 async def process_chatbot_response(result, params, data, model_config, modelOutputConfig):
 
 
@@ -35,10 +35,9 @@ async def process_chatbot_response(result, params, data, model_config, modelOutp
         variables =  { "actions" : data.get('actions') or {}, "user_reference": user_reference, "user_contains": user_contains, "function_calls": function_calls}
         thread_id =  f"{data.get('thread_id') or random_id}-{data.get('sub_thread_id') or random_id}"
         response = await call_ai_middleware(user, bridge_id = bridge_id, variables = variables, thread_id = thread_id)
-        response['response']['data'] = response
-       
-        _.set_(result['modelResponse'], modelOutputConfig.get('message'), response['response']['data'])
-        result['historyParams']['chatbot_message'] = response['response']['data']
+        response = json.dumps(response)
+        _.set_(result['modelResponse'], modelOutputConfig.get('message'), response)
+        result['historyParams']['chatbot_message'] = response
         return
             
     except Exception as err:
