638e9780c3e7080bb53bb55a4e9734db87771f19
ViaSocket-Git <developers@whozzat.com>
2025-05-21 17:01:13 +0530

    removed route

--- src/routes/templateroute.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Request, Depends
-from ..middlewares.middleware import jwt_middleware
-from src.controllers.template_controller import return_template
-router = APIRouter()
-
-@router.get('/{template_id}', dependencies=[Depends(jwt_middleware)])
-async def get_template(request: Request, template_id: str):
-    # Process template_id here if needed
-    return await return_template(request, template_id)
