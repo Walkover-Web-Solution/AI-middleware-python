eefa79bd97b1bc9b7295c74f19bf166b72b1d13e
Yogesh Patel <ygurjar932@gmail.com>
2025-06-10 18:30:26 +0530

    added query

--- src/services/utils/formatter/ai_middleware_chat_api.py
+++ src/services/utils/formatter/ai_middleware_chat_api.py
@@ -5,7 +5,7 @@ from ..ai_call_util import call_ai_middleware
 async def structured_output_optimizer(request):
     try:
         body = await request.json()
-        variables = {'json_schema': body.get('json_schema')}
+        variables = {'json_schema': body.get('json_schema'),'query':body.get('query')}
         user = 'create the json shcmea accroding to the dummy json explained in system prompt.'
         result = await call_ai_middleware(user, bridge_id = bridge_ids['structured_output_optimizer'], variables = variables)
         return result
