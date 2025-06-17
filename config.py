63e2055b2bfd68811d0ad6c70b8a4ca3fc74cb34
Natwar589 <natwarrathor961@gmail.com>
2025-06-17 11:15:02 +0530

    docstar emebed token

--- config.py
+++ config.py
@@ -52,3 +52,4 @@ class Config:
     TRIGGER_PROJECT_ID = os.getenv('TRIGGER_PROJECT_ID', '')
     CHATBOT_ACCESS_KEY = os.getenv('Chatbot_Access_key')
     LOG_QUEUE_NAME = os.getenv('LOG_QUEUE_NAME')
+    DOCSTAR_ACCESS_KEY = os.getenv('DOCSTAR_ACCESS_KEY')
